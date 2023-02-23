128698d0bfb2cbc9479a526ff2ae118fe51d53a5
JuanPa28 <juanpramirez18@gmail.com>
2023-02-22 22:47:56 -0500

    Commit punto 8

--- app.py
+++ app.py
@@ -24,3 +24,10 @@ aprobados = [aprobado for aprobado in new_student if aprobado.avarage_grade() >=
 print("\n")
 for student in aprobados:
     print("El estudiante ", student.name ,"aprobó ya que su nota fue", student.avarage_grade())
+
+aprobados_dict = {aproba2.name: aproba2 for aproba2 in new_student if aproba2.avarage_grade() >= nota_minima}
+
+print("\n")
+print("Lista de los estudiantes aprobados con sus respectivas notas:")
+for name, student in aprobados_dict.items():
+    print(name, student)
