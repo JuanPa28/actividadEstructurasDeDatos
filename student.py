ecf55ae5278748576c71024d871d9e1c06247938
JuanPa28 <juanpramirez18@gmail.com>
2023-02-22 21:56:28 -0500

    Commit punto 5

--- student.py
+++ student.py
@@ -11,4 +11,10 @@ class Student:
         promedio= sum(self.grades) / len(self.grades)
         return promedio
 
-
+    info_students = [
+        {"nombre": "Juan", "edad": 20, "grades": [5, 4.9, 4.2, 4.1, 4.7]},
+        {"nombre": "Pablo", "edad": 19, "grades": [3.2, 3, 2.9, 5, 4.8]},
+        {"nombre": "Maria", "edad": 18, "grades": [5, 5, 5, 5, 5]},
+        {"nombre": "Karla", "edad": 22, "grades": [0.9, 3.5, 2.4, 4, 1.1]},
+        {"nombre": "Dorlan", "edad": 25, "grades": [1, 1, 1, 0.9, 5]},
+    ]
